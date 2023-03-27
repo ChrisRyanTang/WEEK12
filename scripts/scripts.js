338a18a44d8c55d09d38677cbd9c44f7c0774b13
Chris <random@email.com>
2023-03-27 16:17:03 -0700

    colouring buttons

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,43 +1,53 @@
-let buttonPress = document.querySelector('#click');
+// let buttonPress = document.querySelector('#click');
 
-function alertUser() {
-    buttonPress.removeEventListener("click", alertUser);
+// function alertUser() {
+//     buttonPress.removeEventListener("click", alertUser);
+// }
+
+
+
+// buttonPress.addEventListener("click", alertUser, {once: true});
+
+// function colorChange() {
+//     document.body.style.backgroundColor = 'pink';
+// }
+
+
+// buttonPress.addEventListener("click", colorChange);
+
+// function changeText() {
+// // 1. if the button says "click me" change the txtContent to Clicked
+// if (buttonPress.textContent === "Click me!") {
+//     buttonPress.textContent = "Clicked";
+// } else {
+//     buttonPress.textContent = "Click me!";
+// }
+// // 2. else if it says CLicked updaate it back to Click me
+
+// }
+
+// buttonPress.addEventListener("click", changeText);
+
+
+
+
+// function updateImage(event) {
+//     const image = document.querySelector("#shoppingCart");
+//     image.setAttribute("src", 'images/shopping.png');
+//     image.setAttribute('width', 50);
+//     image.setAttribute('height', 50);
+//     image.setAttribute('alt', 'stealing food')
+// }
+
+// updateImage();
+
+const buttonContainer = document.querySelector(".button-container");
+
+function changeBG(event) {
+    if (event.target.tagname === 'BUTTON'){
+        event.target.style.backgroundColor = 'pink';
+    }
+
 }
 
-
-
-buttonPress.addEventListener("click", alertUser, {once: true});
-
-function colorChange() {
-    document.body.style.backgroundColor = 'pink';
-}
-
-
-buttonPress.addEventListener("click", colorChange);
-
-function changeText() {
-// 1. if the button says "click me" change the txtContent to Clicked
-if (buttonPress.textContent === "You clicked?") {
-    buttonPress.textContent = "Clicked";
-} else {
-    buttonPress.textContent = "Click me!";
-}
-// 2. else if it says CLicked updaate it back to Click me
-
-}
-
-buttonPress.addEventListener("click", changeText);
-
-
-
-
-function updateImage() {
-    const image = document.querySelector("#shoppingCart");
-    image.setAttribute("src", 'images/shopping.png');
-    image.setAttribute('width', 200);
-    image.setAttribute('height', 200);
-    image.setAttribute('alt', 'stealing food')
-}
-
-updateImage();
-
+buttonContainer.addEventListener("click", changeBG);
