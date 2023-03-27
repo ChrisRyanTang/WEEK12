6b271a7eb6d88fd7ed6e04d8b85a792818188f95
Chris <random@email.com>
2023-03-27 15:54:23 -0700

    added a downloaded image

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,43 @@
 let buttonPress = document.querySelector('#click');
 
 function alertUser() {
-    alert("You clicked?");
-    // buttonPress.removeEventListener("click", alertUser);
+    buttonPress.removeEventListener("click", alertUser);
 }
 
 
+
 buttonPress.addEventListener("click", alertUser, {once: true});
+
+function colorChange() {
+    document.body.style.backgroundColor = 'pink';
+}
+
+
+buttonPress.addEventListener("click", colorChange);
+
+function changeText() {
+// 1. if the button says "click me" change the txtContent to Clicked
+if (buttonPress.textContent === "You clicked?") {
+    buttonPress.textContent = "Clicked";
+} else {
+    buttonPress.textContent = "Click me!";
+}
+// 2. else if it says CLicked updaate it back to Click me
+
+}
+
+buttonPress.addEventListener("click", changeText);
+
+
+
+
+function updateImage() {
+    const image = document.querySelector("#shoppingCart");
+    image.setAttribute("src", 'images/shopping.png');
+    image.setAttribute('width', 200);
+    image.setAttribute('height', 200);
+    image.setAttribute('alt', 'stealing food')
+}
+
+updateImage();
 
